dac2dc7512ba3b5cc8008c31974be3700ec70c51
marcinwol <wolskint@o2.pl>
2015-04-01 07:22:05 +0800

    program options added.

--- app/mwdcmconverter.cpp
+++ app/mwdcmconverter.cpp
@@ -2,7 +2,6 @@
 
 MwDcmConverter::MwDcmConverter(int acc, char *avv[])
 {
-    cout << "This is dcmconveter" << endl;
     ParseOptions(acc, avv);
 }
 
@@ -10,26 +9,23 @@ MwDcmConverter::MwDcmConverter(int acc, char *avv[])
 void
 MwDcmConverter::ParseOptions(int acc, char *avv[])
 {
-    cout << "Parse options" << endl;
-
-
 
     po::positional_options_description p;
     p.add("in-dir", -1);
 
-
-
-    po::options_description desc("Allowed options");
+    po::options_description desc("mwdcmconverter - allowed options");
 
     desc.add_options()
             ("help,h", po::value<bool>()->implicit_value(true),
                       "produce help message")
-            ("in-dir,i",  po::value<vector<string>>()->multitoken(), "input folder")
-            ("out-dir,o", po::value<string>(), "Location where the found images will be copied")
-            ("verbose,v", po::bool_switch()->default_value(false),
-                            "verbose output")
+            ("in-dir,i",  po::value<vector<string>>()->multitoken(),
+                        "input folder")
+            ("out-dir,o", po::value<string>(),
+                        "location where the found images will be copied")
             ("csv-file,c", po::value<string>(),
-                          "output csv file path");
+                         "output csv file path")
+            ("verbose,v", po::bool_switch()->default_value(false),
+                         "verbose output");
 
 
     po::store(po::command_line_parser(acc, avv)
@@ -39,7 +35,9 @@ MwDcmConverter::ParseOptions(int acc, char *avv[])
 
     po::notify(vm);
 
-
+    if (vm.count("help")) {
+        cout << desc << "\n";
+    }
 }
 
 MwDcmConverter::~MwDcmConverter()
